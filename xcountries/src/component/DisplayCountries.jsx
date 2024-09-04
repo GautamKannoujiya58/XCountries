7521d71d4147cd99ec7ab9aa81c26465293cb46d
gautamKumar <gautamkannoujiya58@gmail.com>
2024-09-05 00:34:11 +0530

    handle API error console statements

--- xcountries/src/component/DisplayCountries.jsx
+++ xcountries/src/component/DisplayCountries.jsx
@@ -2,29 +2,45 @@ import React, { useEffect, useState } from "react";
 import CountryCard from "./CountryCard";
 function DisplayCountries() {
   const [countryData, setCountryData] = useState([]);
+  const [error, setError] = useState(null);
   const fetchData = () => {
-    fetch(`https://xcountries-backend.azurewebsites.net/all`)
-      .then((res) => res.json())
+    fetch(`https://xcountries-backend.azurewebsites.net/l`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status:${res.status}`);
+        }
+
+        return res.json();
+      })
       .then((data) => setCountryData(data))
-      .catch((error) => console.log("Error >>>", error.message));
+      .catch((error) => {
+        console.log("Error >>>", error.message);
+        setError(error.message);
+        // throw error;
+      });
   };
   useEffect(() => {
     fetchData();
   }, []);
 
   // console.log("CountryData >>>", countryData);
+  console.log("ERRRRRRRR >>>>>>", error);
   return (
     <>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {countryData.map((country) => (
-          <CountryCard
-            key={country.abbr}
-            flag={country.flag}
-            name={country.name}
-            abbr={country.abbr}
-          />
-        ))}
-      </div>
+      {!error ? (
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {countryData.map((country) => (
+            <CountryCard
+              key={country.abbr}
+              flag={country.flag}
+              name={country.name}
+              abbr={country.abbr}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>{error}</h3>
+      )}
     </>
   );
 }
